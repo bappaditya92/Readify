57e5711c7a255df6b84e5611b109cf61920120eb
Bappaditya Sinha <bappadityasinha.92@gmail.com>
2025-03-20 23:38:14 +0530

    adding global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
-import express from 'express';
+import express, { NextFunction } from 'express';
+import { HttpError } from 'http-errors';
+import { config } from './config/config';
 
 const app = express();
 
@@ -8,5 +10,20 @@ const app = express();
 app.get('/', (req,res)=>{
     res.json({"sName": "Rahul", "dept": "Civil", "year": 2025})
 })
+
+
+//global error handler
+
+app.use((err:HttpError,req:Request,res:Response,next:NextFunction)=>{
+    const statusCode = err.statusCode || 500;
+
+   return res.status(statusCode).json({
+    message: err.message,
+    errorStack: config.env ==='development'? err.stack: "",
+
+   })
+
+
+}); 
 
 export default app;
