032f8a05dd1079981a2b9b39ba13df8d5793cc13
Bappaditya Sinha <bappadityasinha.92@gmail.com>
2025-03-20 23:10:02 +0530

    adding database setup

--- server.ts
+++ server.ts
@@ -1,7 +1,10 @@
 import {config} from "./src/config/config"
 import app from "./src/app";
+import dbConnect from "./src/config/db";
 
-const startServer = ()=>{
+const startServer = async()=>{
+    //connect Database
+    await dbConnect();
     const port = config.port || 3000;
     app.listen(port, ()=>{
         console.log(`server is running on ${port}`);
