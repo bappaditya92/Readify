6af0e190e1a5277f94ee5ff30cc36e4c97a5c751
Bappaditya Sinha <bappadityasinha.92@gmail.com>
2025-03-21 00:28:12 +0530

    some changes in middleware file

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,21 @@
-import express, { NextFunction, Request, Response} from 'express';
-import  { HttpError } from 'http-errors';
-import { config } from './config/config';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import express, { NextFunction, Request, Response } from "express";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
 
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { config } from "./config/config";
 const app = express();
 
 
 //Routes
 //Http method : Get,POST,PUT,DELETE
-app.get('/', (req,res)=>{
-    // throw new error("something went wrong...")
-    const error = createHttpError(400, "someting went wrong")
-    throw error;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.get('/', (req,res,next)=>{
     res.json({message: "welcome to elib api's"})
-})
-
+});
 
 //global error handler
+app.use(globalErrorHandler);
 
-app.use((err: HttpError,req:Request,res:Response,next:NextFunction)=>{
-    const statusCode = err.statusCode || 500;
-
-   return res.status(statusCode).json({
-    message: err.message,
-    errorStack: config.env ==='development'? err.stack: "",
-
-   })
-
-
-}); 
 
 export default app;
