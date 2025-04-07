cc7e802ca44d801557157d8f0f719fd21353eb34
Bappaditya Sinha <bappadityasinha.92@gmail.com>
2025-04-07 16:53:26 +0530

    added jwtToken

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,5 +4,6 @@ const _config = {
   port: process.env.PORT,
   databaseUrl: process.env.MONGO_CONNECTION_STRING,
   env: process.env.NODE_DEV,
+  jwtSecret: process.env.JWT_SECTRET,
 };
 export const config = Object.freeze(_config);
--- src/users/userController.ts
+++ src/users/userController.ts
@@ -2,6 +2,9 @@ import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
 import bcrypt from "bcrypt";
+//import { User } from "./userTypes";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -19,9 +22,18 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   //password --> hased
   const hashedPassword = await bcrypt.hash(password, 12);
 
-  //process
+  const newUser = await userModel.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+  //Token generation(JWT)
+  const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+    expiresIn: "7d",
+  });
+
   //Response
-  res.json({ message: "User created sucessfully..." });
+  res.json({ accessToekn: token });
 };
 
 export { createUser };
