38dc1f5543522d04858dac7637e497a50345c097
Bappaditya Sinha <bappadityasinha.92@gmail.com>
2025-04-08 17:37:21 +0530

    file upload setup

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,8 +1,26 @@
+import path from "node:path";
 import { Request, Response, NextFunction } from "express";
+
+import cloudinary from "../config/cloudinary";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
   console.log("files", req.files);
+  const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+  const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
+  const fileName = files.coverImage[0].filename;
+  const filePath = path.resolve(
+    __dirname,
+    "../../public/data/uploads",
+    fileName
+  );
+  //application pdf
+  const uploadResult = cloudinary.uploader.upload(filePath, {
+    finame_override: fileName,
+    folder: "book-cover",
+    format: coverImageMimeType,
+  });
+  console.log("uploadresult: ", uploadResult);
 
-  res.json({ message: "book created succesfully" });
+  res.json({});
 };
 export { createBook };
